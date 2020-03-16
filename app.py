9381fb68c4b879f49ae1c5aa76ba7b277d0f03e2
Yenthe Huysmans <huysmye@cronos.be>
2020-03-16 13:43:36 +0100

    adding the data endpoint

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import os
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     target = os.environ.get('TARGET', 'World')
-    return 'Hello {}!\n'.format(target)
+    return 'test {}!\n'.format(target)
+
+@app.route('/data',  methods = ['GET', 'POST'])
+def testdata():
+    if request.method == "GET":
+        return 'dit is een test endpoint'
+
+    if request.method == "POST":
+        return request.json()
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
